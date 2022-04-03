7fd5fd593bd823d9cd94eeba753beaa6f8386074
par-ft <100035078+par-ft@users.noreply.github.com>
2022-04-03 22:55:28 +0430

    all test done (edition2)

--- HW3/AP1400-2-HW3-main/include/bst.h
+++ HW3/AP1400-2-HW3-main/include/bst.h
@@ -1,5 +1,61 @@
 #ifndef BST_H
 #define BST_H
 
+#include <iostream>
+#include <functional>
+#include <initializer_list>
 
-#endif //BST_H
+class BST
+{
+public:
+    class Node
+    {
+    public:
+        Node(int value, Node *left, Node *right) : value{value}, left{left}, right{right} {}
+        Node() : Node{0, nullptr, nullptr} {}
+        Node(const Node &node) : value{node.value}, left{node.left}, right{node.right} {}
+
+        bool operator<(const int &v) const { return value < v; }
+        bool operator>(const int &v) const { return value > v; }
+        bool operator==(const int &v) const { return value == v; }
+        bool operator<=(const int &v) const { return value <= v; }
+        bool operator>=(const int &v) const { return value >= v; }
+
+        friend bool operator<(const int &v, const BST::Node &node) { return v < node.value; }
+        friend bool operator>(const int &v, const BST::Node &node) { return v > node.value; }
+        friend bool operator==(const int &v, const BST::Node &node) { return v == node.value; }
+        friend bool operator<=(const int &v, const BST::Node &node) { return v <= node.value; }
+        friend bool operator>=(const int &v, const BST::Node &node) { return v >= node.value; }
+
+        int value;
+        Node *left;
+        Node *right;
+    };
+
+    Node *&get_root() { return root; };
+    void bfs(std::function<void(Node *&node)> func) const;
+    size_t length() const;
+    bool add_node(int value);
+    Node **find_node(int value);
+    Node **find_parrent(int value);
+    Node **find_successor(int value);
+    bool delete_node(int value);
+
+    BST() = default;
+    ~BST();
+    BST(const BST &bst);
+    BST &operator=(const BST &bst);
+    BST &operator=(BST &&bst);
+    BST(BST &&bst);
+    BST(std::initializer_list<int> args);
+
+    const BST &operator++();
+    const BST operator++(int);
+
+private:
+    Node *root;
+};
+
+std::ostream &operator<<(std::ostream &os, const BST::Node &node);
+std::ostream &operator<<(std::ostream &os, const BST &bst);
+#endif // BST_H
