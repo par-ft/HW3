cb1b8b0f47001ccaef3978f9eb11b3fc72a302e2
par-ft <100035078+par-ft@users.noreply.github.com>
2022-04-03 21:15:28 +0430

    tst 24

--- HW3/AP1400-2-HW3-main/src/unit_test.cpp
+++ HW3/AP1400-2-HW3-main/src/unit_test.cpp
@@ -3,8 +3,8 @@
 #include "gmock/gmock.h"
 #include "bst.h"
 
-TEST(HW3Test, TEST1)
-{
+/*
+TEST(HW3Test, TEST1) {
 
     BST::Node left{10, nullptr, nullptr};
     BST::Node right{10, nullptr, nullptr};
@@ -13,8 +13,7 @@ TEST(HW3Test, TEST1)
     EXPECT_EQ(node.right, &right);
 }
 
-TEST(HW3Test, TEST2)
-{
+TEST(HW3Test, TEST2) {
 
     BST::Node left{10, nullptr, nullptr};
     BST::Node right{10, nullptr, nullptr};
@@ -25,8 +24,7 @@ TEST(HW3Test, TEST2)
     EXPECT_EQ(copy.right, node.right);
 }
 
-TEST(HW3Test, TEST3)
-{
+TEST(HW3Test, TEST3) {
 
     BST::Node default_{};
     EXPECT_EQ(default_.value, 0);
@@ -34,8 +32,7 @@ TEST(HW3Test, TEST3)
     EXPECT_EQ(default_.right, nullptr);
 }
 
-TEST(HW3Test, TEST4)
-{
+TEST(HW3Test, TEST4) {
 
     BST::Node left{10, nullptr, nullptr};
     BST::Node right{10, nullptr, nullptr};
@@ -50,8 +47,7 @@ TEST(HW3Test, TEST4)
     std::cout << std::string(80, '*') << std::endl;
 }
 
-TEST(HW3Test, TEST5)
-{
+TEST(HW3Test, TEST5) {
     BST::Node node{5, nullptr, nullptr};
 
     EXPECT_TRUE(node > 4);
@@ -75,16 +71,15 @@ TEST(HW3Test, TEST5)
     EXPECT_FALSE(2 >= node);
     EXPECT_TRUE(5 == node);
     EXPECT_FALSE(3 == node);
+
 }
 
-TEST(HW3Test, TEST6)
-{
+TEST(HW3Test, TEST6) {
     BST bst{};
     EXPECT_EQ(bst.get_root(), nullptr);
 }
 
-TEST(HW3Test, TEST7)
-{
+TEST(HW3Test, TEST7) {
     BST bst{};
     bst.add_node(10);
     bst.add_node(20);
@@ -94,8 +89,7 @@ TEST(HW3Test, TEST7)
     EXPECT_EQ(bst.get_root()->right->value, 20);
 }
 
-TEST(HW3Test, TEST8)
-{
+TEST(HW3Test, TEST8) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -110,19 +104,17 @@ TEST(HW3Test, TEST8)
     EXPECT_EQ(bst.get_root()->right->right->value, 53);
 }
 
-TEST(HW3Test, TEST9)
-{
+TEST(HW3Test, TEST9) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
     bst.add_node(50);
     bst.add_node(53);
-
+    
     EXPECT_FALSE(bst.add_node(50));
 
     std::vector<int> values;
-    bst.bfs([&values](BST::Node *&node)
-            { values.push_back(node->value); });
+    bst.bfs([&values](BST::Node*& node){values.push_back(node->value);});
     EXPECT_EQ(values.size(), 4);
     EXPECT_TRUE(std::find(values.begin(), values.end(), 25) != values.end());
     EXPECT_TRUE(std::find(values.begin(), values.end(), 10) != values.end());
@@ -130,8 +122,7 @@ TEST(HW3Test, TEST9)
     EXPECT_TRUE(std::find(values.begin(), values.end(), 53) != values.end());
 }
 
-TEST(HW3Test, TEST10)
-{
+TEST(HW3Test, TEST10) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -143,8 +134,7 @@ TEST(HW3Test, TEST10)
     EXPECT_EQ(bst.length(), 6);
 }
 
-TEST(HW3Test, TEST11)
-{
+TEST(HW3Test, TEST11) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -157,8 +147,7 @@ TEST(HW3Test, TEST11)
     std::cout << bst << std::endl;
 }
 
-TEST(HW3Test, TEST12)
-{
+TEST(HW3Test, TEST12) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -166,15 +155,14 @@ TEST(HW3Test, TEST12)
     bst.add_node(53);
     bst.add_node(15);
     bst.add_node(7);
-
-    BST::Node **node{bst.find_node(10)};
+    
+    BST::Node** node{bst.find_node(10)};
     EXPECT_EQ((*node)->value, 10);
     EXPECT_EQ((*node)->left->value, 7);
     EXPECT_EQ((*node)->right->value, 15);
 }
 
-TEST(HW3Test, TEST13)
-{
+TEST(HW3Test, TEST13) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -182,13 +170,12 @@ TEST(HW3Test, TEST13)
     bst.add_node(53);
     bst.add_node(15);
     bst.add_node(7);
-
-    BST::Node **node{bst.find_node(11)};
+    
+    BST::Node** node{bst.find_node(11)};
     EXPECT_EQ(node, nullptr);
 }
 
-TEST(HW3Test, TEST14)
-{
+TEST(HW3Test, TEST14) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -196,17 +183,16 @@ TEST(HW3Test, TEST14)
     bst.add_node(53);
     bst.add_node(15);
     bst.add_node(7);
-
+    
     std::cout << "here" << std::endl;
-    BST::Node **node{bst.find_parrent(15)};
+    BST::Node** node{bst.find_parrent(15)};
     std::cout << "here2" << std::endl;
     EXPECT_EQ((*node)->value, 10);
     EXPECT_EQ((*node)->left->value, 7);
     EXPECT_EQ((*node)->right->value, 15);
 }
 
-TEST(HW3Test, TEST15)
-{
+TEST(HW3Test, TEST15) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -216,15 +202,14 @@ TEST(HW3Test, TEST15)
     bst.add_node(7);
     bst.add_node(8);
     bst.add_node(9);
-
-    BST::Node **node{bst.find_successor(10)};
+    
+    BST::Node** node{bst.find_successor(10)};
     EXPECT_EQ((*node)->value, 9);
     EXPECT_EQ((*node)->left, nullptr);
     EXPECT_EQ((*node)->right, nullptr);
 }
 
-TEST(HW3Test, TEST16)
-{
+TEST(HW3Test, TEST16) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -234,12 +219,11 @@ TEST(HW3Test, TEST16)
     bst.add_node(7);
     bst.add_node(8);
     bst.add_node(9);
-
+    
     EXPECT_FALSE(bst.delete_node(11));
 }
 
-TEST(HW3Test, TEST17)
-{
+TEST(HW3Test, TEST17) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -249,15 +233,14 @@ TEST(HW3Test, TEST17)
     bst.add_node(7);
     bst.add_node(8);
     bst.add_node(9);
-
+    
     EXPECT_EQ(bst.length(), 8);
     EXPECT_TRUE(bst.delete_node(53)); // leaf node
     EXPECT_EQ(bst.length(), 7);
     EXPECT_EQ(bst.get_root()->right->right, nullptr);
 }
 
-TEST(HW3Test, TEST18)
-{
+TEST(HW3Test, TEST18) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -269,7 +252,7 @@ TEST(HW3Test, TEST18)
     bst.add_node(2);
     bst.add_node(7);
     bst.add_node(75);
-
+    
     EXPECT_EQ(bst.length(), 10);
     EXPECT_TRUE(bst.delete_node(10)); // only left child
     EXPECT_EQ(bst.length(), 9);
@@ -280,8 +263,7 @@ TEST(HW3Test, TEST18)
     EXPECT_EQ(bst.get_root()->left->right->right, nullptr);
 }
 
-TEST(HW3Test, TEST19)
-{
+TEST(HW3Test, TEST19) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -293,21 +275,21 @@ TEST(HW3Test, TEST19)
     bst.add_node(2);
     bst.add_node(7);
     bst.add_node(75);
-
-    BST::Node *address_of_5{*bst.find_node(5)};
-    BST::Node *address_of_2{*bst.find_node(2)};
+    
+    BST::Node* address_of_5{*bst.find_node(5)};
+    BST::Node* address_of_2{*bst.find_node(2)};
 
     EXPECT_TRUE(bst.delete_node(10)); // only left child
 
-    // the remainig nodes should not be regenerated
+    // the remainig nodes should not be regenerated 
     // => address of remaning nodes before and after deletion should be the same
     // when deleting a node: just reconnect the needed parrents and childs
     EXPECT_EQ(*bst.find_node(5), address_of_5);
     EXPECT_EQ(*bst.find_node(2), address_of_2);
+    
 }
 
-TEST(HW3Test, TEST20)
-{
+TEST(HW3Test, TEST20) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -319,7 +301,7 @@ TEST(HW3Test, TEST20)
     bst.add_node(2);
     bst.add_node(7);
     bst.add_node(75);
-
+    
     EXPECT_EQ(bst.length(), 10);
     EXPECT_TRUE(bst.delete_node(50)); // only right child
     EXPECT_EQ(bst.length(), 9);
@@ -331,8 +313,7 @@ TEST(HW3Test, TEST20)
     EXPECT_EQ(bst.get_root()->right->left->right, nullptr);
 }
 
-TEST(HW3Test, TEST21)
-{
+TEST(HW3Test, TEST21) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -344,21 +325,20 @@ TEST(HW3Test, TEST21)
     bst.add_node(2);
     bst.add_node(7);
     bst.add_node(75);
-
-    BST::Node *address_of_60{*bst.find_node(60)};
-    BST::Node *address_of_75{*bst.find_node(75)};
+    
+    BST::Node* address_of_60{*bst.find_node(60)};
+    BST::Node* address_of_75{*bst.find_node(75)};
 
     EXPECT_TRUE(bst.delete_node(50)); // only left child
 
-    // the remainig nodes should not be regenerated
+    // the remainig nodes should not be regenerated 
     // => address of remaning nodes before and after deletion should be the same
     // when deleting a node: just reconnect the needed parrents and childs
     EXPECT_EQ(*bst.find_node(60), address_of_60);
     EXPECT_EQ(*bst.find_node(75), address_of_75);
 }
 
-TEST(HW3Test, TEST22)
-{
+TEST(HW3Test, TEST22) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -372,7 +352,7 @@ TEST(HW3Test, TEST22)
     bst.add_node(75);
     bst.add_node(20);
     bst.add_node(15);
-
+    
     EXPECT_EQ(bst.length(), 12);
     EXPECT_TRUE(bst.delete_node(10)); // both children exist
     EXPECT_EQ(bst.length(), 11);
@@ -381,8 +361,7 @@ TEST(HW3Test, TEST22)
     EXPECT_EQ(bst.get_root()->left->left->value, 5);
 }
 
-TEST(HW3Test, TEST23)
-{
+TEST(HW3Test, TEST23) {
     BST bst{};
     bst.add_node(25);
     bst.add_node(10);
@@ -397,7 +376,7 @@ TEST(HW3Test, TEST23)
     bst.add_node(20);
     bst.add_node(15);
     bst.add_node(22);
-
+    
     EXPECT_EQ(bst.length(), 13);
     EXPECT_TRUE(bst.delete_node(25)); // both children exist
     EXPECT_EQ(bst.length(), 12);
@@ -406,8 +385,7 @@ TEST(HW3Test, TEST23)
     EXPECT_EQ(bst.get_root()->left->right->right, nullptr);
 }
 
-TEST(HW3Test, TEST24)
-{
+TEST(HW3Test, TEST24) {
     BST bst{};
     bst.add_node(5);
     bst.add_node(10);
@@ -416,13 +394,13 @@ TEST(HW3Test, TEST24)
     EXPECT_EQ(bst.length(), copy.length());
     EXPECT_EQ(bst.get_root()->right->value, copy.get_root()->right->value);
 }
-/*
+
 TEST(HW3Test, TEST25) {
     BST bst{};
     bst.add_node(5);
     bst.add_node(10);
     bst.add_node(50);
-
+    
     BST equal{};
     bst.add_node(51);
     bst.add_node(66);
@@ -442,7 +420,7 @@ TEST(HW3Test, TEST26) {
     bst.add_node(4);
     bst.add_node(60);
     bst.add_node(8);
-
+    
     bst = bst;
     EXPECT_EQ(bst.get_root()->right->left->value, 8);
 }
@@ -452,7 +430,7 @@ TEST(HW3Test, TEST27) {
     bst.add_node(5);
     bst.add_node(10);
     bst.add_node(50);
-
+    
     int test_value{bst.get_root()->right->value};
     BST::Node* adddress_of_root{*bst.find_node(5)};
 
@@ -470,7 +448,7 @@ TEST(HW3Test, TEST28) {
     bst.add_node(5);
     bst.add_node(20);
     bst.add_node(22);
-
+    
     int test_value{bst.get_root()->left->right->value};
     BST::Node* adddress_of_root{*bst.find_node(25)};
 
@@ -510,6 +488,9 @@ TEST(HW3Test, TEST31) {
     bst2.bfs([&values2](BST::Node*& node){values2.push_back(node->value);});
 
     for(size_t i{}; i < values1.size(); i++)
-        EXPECT_EQ(values2[i], values1[i]-1);
+        EXPECT_EQ(values2[i], values1[i]-1);    
 }
 */
+
+
+
--- HW3/src/bst.cpp
+++ HW3/src/bst.cpp
@@ -1,1 +1,317 @@
 #include "bst.h"
+
+#include <iomanip>
+#include <queue>
+#include <string>
+
+std::ostream &operator<<(std::ostream &os, const BST::Node &node)
+{
+    os << "\u001B[32m" << std::left << std::setw(17) << &node << "\u001B[0m=> value:\u001B[32m"
+       << std::setw(10) << node.value << "\u001B[0mleft:\u001B[32m" << std::setw(16) << node.left
+       << "\u001B[0mright:\u001B[32m" << node.right << "\u001B[0m";
+
+    return os;
+}
+
+bool BST::add_node(int value)
+{
+    if (!root)
+    {
+        root = new Node(value, nullptr, nullptr);
+        return true;
+    }
+
+    Node *temp{root};
+    bool right_child{};
+    Node *parrent{nullptr};
+
+    while (true)
+    {
+        if (*temp == value)
+            return false;
+
+        else if (*temp > value)
+        {
+            right_child = false;
+            parrent = temp;
+            temp = temp->left;
+        }
+
+        else
+        {
+            right_child = true;
+            parrent = temp;
+            temp = temp->right;
+        }
+
+        if (!temp)
+        {
+            temp = new Node(value, nullptr, nullptr);
+
+            if (right_child)
+                parrent->right = temp;
+            else
+                parrent->left = temp;
+
+            return true;
+        }
+    }
+}
+
+void BST::bfs(std::function<void(Node *&node)> func) const
+{
+    if (root)
+    {
+        std::queue<Node *> nodes{};
+        nodes.push(root);
+        while (!nodes.empty())
+        {
+            if (nodes.front()->left)
+                nodes.push(nodes.front()->left);
+
+            if (nodes.front()->right)
+                nodes.push(nodes.front()->right);
+
+            func(nodes.front());
+            nodes.pop();
+        }
+    }
+}
+
+size_t BST::length() const
+{
+    size_t cnt{};
+
+    if (root)
+    {
+        std::queue<Node *> nodes{};
+        nodes.push(root);
+
+        while (!nodes.empty())
+        {
+            if (nodes.front()->left)
+                nodes.push(nodes.front()->left);
+
+            if (nodes.front()->right)
+                nodes.push(nodes.front()->right);
+
+            cnt++;
+            nodes.pop();
+        }
+    }
+
+    return cnt;
+}
+
+std::ostream &operator<<(std::ostream &os, const BST &bst)
+{
+    std::string s(80, '*');
+
+    os << s << "\n";
+
+    bst.bfs([&os](BST::Node *&node)
+            { os << *node << "\n"; });
+
+    os << "binary search tree size: \u001B[32m" << bst.length() << "\u001B[0m\n";
+    os << s << std::endl;
+
+    return os;
+}
+
+BST::Node **BST::find_node(int value)
+{
+    if (!root)
+        return nullptr;
+
+    Node **temp{&root};
+
+    while (true)
+    {
+        if (**temp == value)
+            return temp;
+
+        if (**temp > value)
+            temp = &((*temp)->left);
+
+        else
+            temp = &((*temp)->right);
+
+        if (*temp == nullptr)
+            return nullptr;
+    }
+}
+
+BST::Node **BST::find_parrent(int value)
+{
+    if (!root)
+        return nullptr;
+
+    if (!find_node(value))
+        return nullptr;
+
+    Node **temp{&root};
+    Node **parrent{nullptr};
+
+    while (true)
+    {
+        if (**temp == value)
+            return parrent;
+
+        parrent = temp;
+        if (**temp > value)
+            temp = &((*temp)->left);
+
+        else
+            temp = &((*temp)->right);
+    }
+}
+
+BST::Node **BST::find_successor(int value)
+{
+    Node **node{find_node(value)};
+
+    if (!node)
+        return nullptr;
+
+    Node **successor{};
+    if ((*node)->left)
+    {
+        successor = &((*node)->left);
+
+        while ((*successor)->right)
+            successor = &((*successor)->right);
+    }
+    else if ((*node)->right)
+    {
+        successor = &((*node)->right);
+
+        while ((*successor)->left)
+            successor = &((*successor)->left);
+    }
+    else
+        successor = nullptr;
+
+    return successor;
+}
+
+bool BST::delete_node(int value)
+{
+    Node **node{find_node(value)};
+
+    if (!node)
+        return false;
+
+    if (!(*node)->left && !(*node)->right)
+    {
+        Node **parrent{find_parrent((*node)->value)};
+        if (parrent)
+            if ((*parrent)->value > (*node)->value)
+            {
+                delete *node;
+                (*parrent)->left = nullptr;
+            }
+            else
+            {
+                delete *node;
+                (*parrent)->right = nullptr;
+            }
+
+        else // It will be root, because only root has no parrents
+        {
+            delete *node;
+            root = nullptr;
+        }
+    }
+
+    else if (!(*node)->left || !(*node)->right)
+    {
+        Node **parrent{find_parrent((*node)->value)};
+        if (parrent)
+            if ((*parrent)->value > (*node)->value)
+            {
+
+                if ((*node)->left)
+                {
+                    Node *temp{(*node)->left};
+                    delete *node;
+                    (*parrent)->left = temp;
+                }
+                else
+                {
+                    Node *temp{(*node)->right};
+                    delete *node;
+                    (*parrent)->left = temp;
+                }
+            }
+
+            else
+            {
+                if ((*node)->left)
+                {
+                    Node *temp{(*node)->left};
+                    delete *node;
+                    (*parrent)->right = temp;
+                }
+                else
+                {
+                    Node *temp{(*node)->right};
+                    delete *node;
+                    (*parrent)->right = temp;
+                }
+            }
+
+        else // It will be root, because only root has no parrents
+        {
+            if ((*node)->left)
+            {
+                Node *temp{(*node)->left};
+                delete *node;
+                root = temp;
+            }
+            else
+            {
+                Node *temp{(*node)->right};
+                delete *node;
+                root = temp;
+            }
+        }
+    }
+
+    else
+    {
+        Node **successor{find_successor(value)};
+        Node **successor_parrent{find_parrent((*successor)->value)};
+        if ((*successor_parrent)->value > (*successor)->value)
+        {
+            (*node)->value = (*successor)->value;
+            delete *successor;
+            (*successor_parrent)->left = nullptr;
+        }
+        else
+        {
+            (*node)->value = (*successor)->value;
+            delete *successor;
+            (*successor_parrent)->right = nullptr;
+        }
+    }
+
+    return true;
+}
+
+BST::~BST()
+{
+    std::vector<Node *> nodes;
+    bfs([&nodes](Node *&node)
+        { nodes.push_back(node); });
+    for (auto &node : nodes)
+        delete node;
+}
+
+BST::BST(const BST &bst) : root{nullptr}
+{
+    std::vector<Node *> nodes;
+    bst.bfs([&nodes](Node *&node)
+            { nodes.push_back(node); });
+    for (auto &node : nodes)
+        add_node(node->value);
+}
